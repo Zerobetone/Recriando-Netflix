aaaa2ff4d3e8b39e07274e8f12181291e60c3628
Zerobetone <robertolimaepdep5@gmail.com>
2022-12-15 15:12:35 -0300

    Elaboração da Interface Netflix - primeira versão

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "093443301da1294258eaa20cbafb1404"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
